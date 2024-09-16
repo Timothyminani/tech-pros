0a17b72706bd9e4573ae6adf76f8bd6cdb1fb352
Timothyminani <minanitimothy@gmail.com>
2024-09-16 13:34:35 +0300

    techpros project

---
+++ script.js
@@ -0,0 +1,112 @@
+/*CONTACT PAGE*/
+
+const side = document.getElementById('side');
+const nav = document.getElementById('nav');
+const close = document.getElementById('close');
+
+if(side){
+    side.addEventListener('click', function(){
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', function(){
+        nav.classList.remove('active');
+    })
+}
+
+/*HOME PAGE*/
+
+const side1 = document.getElementById('side');
+const nav1 = document.getElementById('nav');
+const close1 = document.getElementById('close');
+
+if(side1){
+    side.addEventListener('click', function(){
+        nav.classList.add('active');
+    })
+}
+
+if(close1){
+    close.addEventListener('click', function(){
+        nav.classList.remove('active');
+    })
+}
+
+/*ABOUT PAGE*/
+
+const side2 = document.getElementById('side');
+const nav2 = document.getElementById('nav');
+const close2 = document.getElementById('close');
+
+if(side2){
+    side.addEventListener('click', function(){
+        nav.classList.add('active');
+    })
+}
+
+if(close2){
+    close.addEventListener('click', function(){
+        nav.classList.remove('active');
+    })
+}
+
+
+/*PRODUCT PAGE*/
+
+const side3 = document.getElementById('side');
+const nav3 = document.getElementById('nav');
+const close3 = document.getElementById('close');
+
+if(side3){
+    side.addEventListener('click', function(){
+        nav.classList.add('active');
+    })
+}
+
+if(close3){
+    close.addEventListener('click', function(){
+        nav.classList.remove('active');
+    })
+}
+
+
+/*SERVICE PAGE*/
+
+const side4 = document.getElementById('side');
+const nav4 = document.getElementById('nav');
+const close4 = document.getElementById('close');
+
+if(side4){
+    side.addEventListener('click', function(){
+        nav.classList.add('active');
+    })
+}
+
+if(close4){
+    close.addEventListener('click', function(){
+        nav.classList.remove('active');
+    })
+}
+
+
+/*Mobile Filter*/
+
+const side5 = document.getElementById('filterM');
+const nav5 = document.getElementById('filter_container_mobile');
+//const close5 = document.getElementById('close');
+
+if(side5){
+    side5.addEventListener('click', function(){
+        nav5.classList.add('open');
+    })
+}
+
+/*if(close5){
+    close.addEventListener('click', function(){
+        nav.classList.remove('open');
+    })
+}
+
+*/
